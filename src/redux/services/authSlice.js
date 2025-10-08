c323ce6151accd6f40db09d8645037f395fda415
ranntharath <tharath8061@gmail.com>
2025-10-08 22:25:15 +0700

    add: register-login

---
+++ src/redux/services/authSlice.js
@@ -0,0 +1,36 @@
+
+import { fetchBaseQuery } from "@reduxjs/toolkit/query";
+import { createApi } from "@reduxjs/toolkit/query/react";
+
+
+export const authApi = createApi({
+  reducerPath: "authApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: import.meta.env.VITE_BACKEND_URL,
+    prepareHeaders: (headers) => {
+      const token = localStorage.getItem("accessToken");
+      if (token) {
+        headers.set("accessToken", `Bearer ${token}`);
+      }
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    register: builder.mutation({
+      query: (userData) => ({
+        url: "/auth/register",
+        method: "POST",
+        body: userData,
+      }),
+    }),
+
+    login: builder.mutation({
+      query: (userData) => ({
+        url: "/auth/login",
+        method: "POST",
+        body: userData,
+      }),
+    }),
+  }),
+});
+export const { useLoginMutation, useRegisterMutation } = authApi;
