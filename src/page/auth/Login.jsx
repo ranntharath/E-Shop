5c77c3821d3c9a1e776d854da95973270e973ad5
ranntharath <tharath8061@gmail.com>
2025-10-11 12:06:47 +0700

    commit test

--- src/page/auth/Login.jsx
+++ src/page/auth/Login.jsx
@@ -37,6 +37,7 @@ function Login() {
   return (
     <>
     test
+    fea
       <div className="min-h-screen flex items-center justify-center bg-gray-50 p-4">
         <div className="w-full max-w-md bg-white rounded-2xl shadow-lg p-8">
           <h2 className="text-2xl font-bold text-center text-gray-800 mb-6">
